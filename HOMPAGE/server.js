841716b915a0d90712168e33e07eec9c14b0d39d
choyuchan <61843057+choyuchan@users.noreply.github.com>
2024-06-10 10:13:58 +0900

    Add files via upload

---
+++ HOMPAGE/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/submit-form', (req, res) => {
+    const { name, email, message } = req.body;
+    console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+    res.send('Form submitted successfully!');
+});
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
